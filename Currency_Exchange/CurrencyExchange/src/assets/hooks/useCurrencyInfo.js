a609670f13063d322a0222c9ff36bc94bb983541
Sanskar <sanskarjaiswal010@gmail.com>
2024-03-15 04:58:23 +0530

    #2

--- Currency_Exchange/CurrencyExchange/src/assets/hooks/useCurrencyInfo.js
+++ Currency_Exchange/CurrencyExchange/src/assets/hooks/useCurrencyInfo.js
@@ -1,11 +1,18 @@
-import { useEffect, useState } from "react";
-let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency)
-{
 
-    useEffect(()={
-
-    }, [])
-
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch("https://openexchangerates.org/api/latest.json?app_id=564a7cbbabd64377a5e24252a71b6694")
+        .then((res) => {if(res.ok) 
+            console.log( res)
+            return res.json()})
+        .then((res) => setData(res["rates"]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
+
+export default useCurrencyInfo;
